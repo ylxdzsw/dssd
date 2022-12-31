19bf7cf84fec9f1e6d10f4dcd711d9b2fc16a622
Shiwei Zhang <ylxdzsw@gmail.com>
2023-01-01 03:13:01 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![allow(clippy::uninlined_format_args)]
 #![allow(clippy::type_complexity)]
+#![allow(non_upper_case_globals)]
+#![feature(let_chains)]
 
 use dbus::{blocking::Connection, arg::{Variant, RefArg, PropMap}, Path, MethodErr};
 use dbus_crossroads::{Crossroads, Context, IfaceToken, IfaceBuilder};
@@ -25,15 +27,14 @@ type Secret = (Path<'static>, Vec<u8>, Vec<u8>, String);
 /// The global singleton storing all states of the application
 #[derive(Serialize, Deserialize, Debug)]
 struct Service {
-    next_sess_id: AtomicU64,
-    next_item_id: AtomicU64,
-    items: BTreeMap<String, Item>
+    next_item_id: u64,
+    items: BTreeMap<u64, Item>
 }
 
 impl Service {
     // The Default trait is not const so we need this one
     const fn new() -> Self {
-        Service { next_sess_id: AtomicU64::new(0), next_item_id: AtomicU64::new(0), items: BTreeMap::new() }
+        Service { next_item_id: 0, items: BTreeMap::new() }
     }
 
     fn save(&self) -> Result<(), Box<dyn Error>> {
@@ -51,17 +52,27 @@ impl Service {
         Ok(())
     }
 
-    fn load(&mut self) -> Result<(), Box<dyn Error>> {
+    fn load(&mut self, cr: &mut Crossroads) -> Result<(), Box<dyn Error>> {
         let config_dir = get_config_directory()?;
         if let Ok(file) = std::fs::File::open(format!("{config_dir}/secrets")) {
             *self = serde_json::from_reader(file)?
         }
+
+        for &item_id in self.items.keys() {
+            let item_id_str = Item::id_to_string(item_id);
+            cr.insert(item_id_str, &[item_iface_token_mutex.lock().unwrap().unwrap()], ItemHandle(item_id));
+        }
+
         Ok(())
     }
-}
 
-#[allow(non_upper_case_globals)]
-static service_mutex: Mutex<Service> = Mutex::new(Service::new());
+    fn find_by_attributes(&self, attributes: &BTreeMap<String, String>) -> Vec<u64> {
+        self.items.iter()
+            .filter(|(_, item)| attributes.iter().all(|(attr, value)| item.attributes.get(attr) == Some(value)))
+            .map(|(&item_id, _)| item_id)
+            .collect()
+    }
+}
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 struct Item {
@@ -74,8 +85,123 @@ struct Item {
     modified: u64
 }
 
+impl Item {
+    fn id_to_string(id: u64) -> String {
+        format!("/org/freedesktop/secrets/collection/Login/{id}")
+    }
+}
+
+static next_sess_id: AtomicU64 = AtomicU64::new(0);
+static service_mutex: Mutex<Service> = Mutex::new(Service::new());
+static service_iface_token_mutex: Mutex<Option<IfaceToken<ServiceHandle>>> = Mutex::new(None);
+static collection_iface_token_mutex: Mutex<Option<IfaceToken<CollectionHandle>>> = Mutex::new(None);
+static item_iface_token_mutex: Mutex<Option<IfaceToken<ItemHandle>>> = Mutex::new(None);
+static session_iface_token_mutex: Mutex<Option<IfaceToken<SessionHandle>>> = Mutex::new(None);
+
+struct ServiceHandle;
+
+impl ServiceHandle {
+    fn open_session(
+        _ctx: &mut Context,
+        cr: &mut Crossroads,
+        (algorithm, input): (String, Variant<Box<dyn RefArg>>)
+    ) -> Result<(Variant<&'static str>, Path<'static>), MethodErr> {
+        eprintln!("{}, {:?}", algorithm, input);
+        if algorithm != "plain" {
+            return Err(dbus::Error::new_custom("org.freedesktop.DBus.Error.NotSupported", "").into())
+        }
+
+        let session_id = next_sess_id.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        let session_id_str = format!("/org/freedesktop/secrets/session/{session_id}");
+        cr.insert(session_id_str.clone(), &[session_iface_token_mutex.lock().unwrap().unwrap()], SessionHandle);
+
+        Ok((Variant(""), Path::new(session_id_str).unwrap()))
+    }
+
+    fn search_item(
+        _ctx: &mut Context,
+        _cr: &mut ServiceHandle,
+        (attributes,): (BTreeMap<String, String>,)
+    ) -> Result<(Vec<Path<'static>>, Vec<Path<'static>>), MethodErr> {
+        eprintln!("Search Item {attributes:?}");
+
+        let service = service_mutex.lock().unwrap();
+        let results: Result<Vec<_>, _> = service.find_by_attributes(&attributes).into_iter()
+            .map(Item::id_to_string)
+            .map(Path::new)
+            .collect();
+
+        eprintln!("Search Item Results {results:?}");
+                    
+        Ok((results.unwrap(), Vec::<Path>::new()))
+    }
+
+    fn register_dbus(cr: &mut Crossroads) {
+        let iface_token = cr.register("org.freedesktop.Secret.Service", |iface_builder| {
+            iface_builder.method_with_cr("OpenSession", ("algorithm", "input"), ("output", "result"), Self::open_session);
+            iface_builder.method("SearchItems", ("attributes",), ("unlocked", "locked"), Self::search_item);
+        });
+
+        *service_iface_token_mutex.lock().unwrap() = Some(iface_token)
+    }
+}
+
+struct CollectionHandle;
+
+impl CollectionHandle {
+    fn create_item(
+        _ctx: &mut Context,
+        cr: &mut Crossroads,
+        (properties, secret, replace): (PropMap, Secret, bool)
+    ) -> Result<(Path<'static>, Path<'static>), MethodErr> {
+
+        eprintln!("Create Item: {properties:?} {secret:?} {replace:?}");
+
+        let label = properties.get("org.freedesktop.Secret.Item.Label")
+            .and_then(|b| b.as_str().map(|x| x.to_string()))
+            .unwrap_or_default();
+        let attributes = properties.get("org.freedesktop.Secret.Item.Attributes")
+            .and_then(|b| Some(
+                dbus::arg::cast::<PropMap>(&b.0)?.iter()
+                    .map(|(k, v)| (k.to_string(), v.as_str().unwrap().to_string()))
+                    .collect::<BTreeMap<String, String>>()
+            ))
+            .unwrap_or_default();
+        
+        let (_, _, content, content_type) = secret;
+
+        let now = get_unix_timestamp();
+        let item = Item { label, attributes, created: now, modified: now, content, content_type };
+
+        let mut service = service_mutex.lock().unwrap();
+
+        let item_id = if replace && let Some(&item_id) = service.find_by_attributes(&item.attributes).first() {
+            item_id
+        } else {
+            let item_id = service.next_item_id;
+            service.next_item_id += 1;
+            item_id
+        };
+
+        service.items.insert(item_id, item);
+
+        let item_id_str = format!("/org/freedesktop/secrets/collection/Login/{item_id}");
+        cr.insert(item_id_str.clone(), &[item_iface_token_mutex.lock().unwrap().unwrap()], ItemHandle(item_id));
+
+        Ok((Path::new(item_id_str).unwrap(), Path::new("/").unwrap()))
+    }
+
+    fn register_dbus(cr: &mut Crossroads) {
+        let iface_token = cr.register("org.freedesktop.Secret.Collection", |iface_builder| {
+            iface_builder.method_with_cr("CreateItem", ("properties", "secret", "replace"), ("item", "prompt"), Self::create_item);
+        });
+
+        *collection_iface_token_mutex.lock().unwrap() = Some(iface_token);
+    }
+}
+
 #[derive(Debug, Clone)]
-struct ItemHandle(String);
+struct ItemHandle(u64);
 
 impl ItemHandle {
     fn with_item<T>(&self, cb: impl FnOnce(&Item) -> T) -> T {
@@ -102,7 +228,7 @@ impl ItemHandle {
     }
 
     fn get_secret(
-        ctx: &mut Context,
+        _ctx: &mut Context,
         item_handle: &mut ItemHandle,
         (sess,): (Path<'static>,)
     ) -> Result<(Secret, ), MethodErr> {
@@ -112,7 +238,7 @@ impl ItemHandle {
     }
 
     fn set_secret(
-        ctx: &mut Context,
+        _ctx: &mut Context,
         item_handle: &mut ItemHandle,
         (secret,): (Secret, )
     ) -> Result<(), MethodErr> {
@@ -124,10 +250,11 @@ impl ItemHandle {
         })
     }
 
-    fn register_dbus(cr: &mut Crossroads) -> IfaceToken<Self> {
-        cr.register("org.freedesktop.Secret.Item", |iface_builder: &mut IfaceBuilder<ItemHandle>| {
+    fn register_dbus(cr: &mut Crossroads) {
+        let iface_token = cr.register("org.freedesktop.Secret.Item", |iface_builder: &mut IfaceBuilder<ItemHandle>| {
             iface_builder.method_with_cr("Delete", (), ("Prompt", ), Self::delete);
             iface_builder.method("GetSecret", ("session", ), ("secret", ), Self::get_secret);
+            iface_builder.method("SetSecret", ("secret", ), (), Self::set_secret);
 
 
             iface_builder.property("Locked")
@@ -152,82 +279,30 @@ impl ItemHandle {
             
             iface_builder.property("Modified")
                 .get(|_, item_handle| item_handle.with_item(|item| Ok(item.modified)));
-        })
+        });
+
+        *item_iface_token_mutex.lock().unwrap() = Some(iface_token);
     }
 }
 
-struct CollectionHandle;
+struct SessionHandle;
 
-impl CollectionHandle {
-    fn create_item(
-        _: &mut Context,
-        _: &mut Crossroads,
-        (properties, secret, replace): (PropMap, Secret, bool)
-    ) -> Result<(Path<'static>, Path<'static>), MethodErr> {
-        let label = properties.get("org.freedesktop.Secret.Item.Label")
-            .and_then(|b| b.as_str().map(|x| x.to_string()))
-            .unwrap_or_default();
-        let attributes = properties.get("org.freedesktop.Secret.Item.Attributes")
-            .and_then(|b| Some(
-                dbus::arg::cast::<PropMap>(&b.0)?.into_iter()
-                    .map(|(k, v)| (k.to_string(), v.as_str().unwrap().to_string()))
-                    .collect::<BTreeMap<String, String>>()
-            ))
-            .unwrap_or_default();
-        
-        let now = get_unix_timestamp();
-        let item = Item { label, attributes, created: now, modified: now, ..Default::default() };
-
-
-        
+impl SessionHandle {
+    fn delete(
+        ctx: &mut Context,
+        cr: &mut Crossroads,
+        (): ()
+    ) -> Result<(), MethodErr> {
+        cr.remove::<Self>(ctx.path()).unwrap();
+        Ok(())
     }
 
+    fn register_dbus(cr: &mut Crossroads) {
+        let iface_token = cr.register("org.freedesktop.Secret.Session", |iface_builder: &mut IfaceBuilder<SessionHandle>| {
+            iface_builder.method_with_cr("Delete", (), (), Self::delete);
+        });
 
-    fn register_dbus(cr: &mut Crossroads) -> IfaceToken<Self> {
-        cr.register("org.freedesktop.Secret.Collection", |iface_builder| {
-            // iface_builder.method()
-        })
-    }
-}
-
-
-struct ServiceHandle;
-
-impl ServiceHandle {
-    fn open_session(
-        _: &mut Context,
-        _: &mut Crossroads,
-        (algorithm, input): (String, Variant<Box<dyn RefArg>>)
-    ) -> Result<(Variant<&'static str>, Path<'static>), MethodErr> {
-        let service = service_mutex.lock().unwrap();
-
-        eprintln!("{}, {:?}", algorithm, input);
-        if algorithm != "plain" {
-            return Err(dbus::Error::new_custom("org.freedesktop.DBus.Error.NotSupported", "").into())
-        }
-        
-        let session_id = service.next_sess_id.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-        let session_id_str = format!("/org/freedesktop/secrets/session/s{session_id}");
-        // cr.insert(session_id_str.clone(), &[], Session::default());
-
-        Ok((Variant(""), Path::new(session_id_str).unwrap()))
-    }
-
-    fn search_item(
-        _: &mut Context,
-        _: &mut ServiceHandle,
-        (attributes,): (BTreeMap<String, String>,)
-    ) -> Result<(Vec<Path<'static>>, Vec<Path<'static>>), MethodErr> {
-        eprintln!("{:?}", attributes);
-                    
-        Ok((Vec::<Path>::new(), Vec::<Path>::new()))
-    }
-
-    fn register_dbus(cr: &mut Crossroads) -> IfaceToken<Self> {
-        cr.register("org.freedesktop.Secret.Service", |iface_builder| {
-            iface_builder.method_with_cr("OpenSession", ("algorithm", "input"), ("output", "result"), Self::open_session);
-            iface_builder.method("SearchItems", ("attributes",), ("unlocked", "locked"), Self::search_item);
-        })
+        *session_iface_token_mutex.lock().unwrap() = Some(iface_token);
     }
 }
 
@@ -247,8 +322,16 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut cr = Crossroads::new();
 
-    let service_iface_token = ServiceHandle::register_dbus(&mut cr);
-    cr.insert("/org/freedesktop/secrets", &[service_iface_token], ServiceHandle);
+    ServiceHandle::register_dbus(&mut cr);
+    CollectionHandle::register_dbus(&mut cr);
+    ItemHandle::register_dbus(&mut cr);
+    SessionHandle::register_dbus(&mut cr);
+
+    cr.insert("/org/freedesktop/secrets", &[service_iface_token_mutex.lock()?.unwrap()], ServiceHandle);
+    cr.insert("/org/freedesktop/secrets/aliases/default", &[collection_iface_token_mutex.lock()?.unwrap()], CollectionHandle);
+    cr.insert("/org/freedesktop/secrets/collection/Login", &[collection_iface_token_mutex.lock()?.unwrap()], CollectionHandle);
+
+    service_mutex.lock().unwrap().load(&mut cr)?;
 
     cr.serve(&c)?;
     unreachable!()
